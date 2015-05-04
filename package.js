eb7093a49b08615ac449d7b827079b311bea9c3b
Hao Tang <haotc@designveloper.com>
2015-05-04 15:28:52 +0700

    Add files to client

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
-ackage.describe({
+Package.describe({
   name: 'dsv:bootstrap3-switch',
-  version: '0.0.1',
+  version: '1.0.0',
 
   // Brief, one-line summary of the package.
   summary: '',
@@ -11,4 +11,17 @@ ackage.describe({
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.0');
+
+  api.addFiles('bootstrap-switch.js', 'client');
+  api.addFiles('bootstrap-switch.css', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+
+  api.addFiles('bootstrap3-swatch-test.js');
 });
