ad38791258e29eeb2ac321a84808bc39dbce7a6c
Hao Tang <haotc@designveloper.com>
2015-05-04 15:14:33 +0700

    Initial commit

---
+++ package.js
@@ -0,0 +1,14 @@
+ackage.describe({
+  name: 'dsv:bootstrap3-switch',
+  version: '0.0.1',
+
+  // Brief, one-line summary of the package.
+  summary: '',
+
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/haotangio/bootstrap3-switch.git',
+
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
